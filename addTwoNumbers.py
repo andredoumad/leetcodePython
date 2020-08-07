60123393c792863ab0c3bd8f5b54a35d641d5b2d
tr3b <andre@stringkeeper.com>
2020-08-06 21:38:27 -0700

    Andre Doumad

--- addTwoNumbers.py
+++ addTwoNumbers.py
@@ -10,61 +10,62 @@ Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 0 -> 8
 Explanation: 342 + 465 = 807.
 '''
-# import typing
+import unittest
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        a=''
-        b=''
-        searching = True
-        while searching:
-            a+=str(l1.val)
+        return self.solveIt(l1, l2, 0)
+        
+    def solveIt(self, l1, l2, c):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        val = l1.val + l2.val + c
+        c = val // 10
+        ret = ListNode(val % 10 ) 
+        
+        if (l1.next != None or l2.next != None or c != 0):
             if l1.next == None:
-                searching = False
-            l1 = l1.next
-
-        searching = True
-        while searching:
-            b+=str(l2.val)
+                l1.next = ListNode(0)
             if l2.next == None:
-                searching = False
-            l2 = l2.next
-        # print(a)
-        # print(b)
-        c = str(int(a) + int(b))
+                l2.next = ListNode(0)
+            ret.next = self.solveIt(l1.next,l2.next,c)
+        return ret
+
+
+class unitTest(unittest.TestCase):
+    def test_a(self):
+        l1_c = ListNode(2, None)
+        l1_b = ListNode(4, l1_c)
+        l1_a = ListNode(3, l1_b)
+
+        l2_c = ListNode(5, None)
+        l2_b = ListNode(6, l2_c)
+        l2_a = ListNode(4, l2_b)
+
+        solution = Solution()
+        result = solution.addTwoNumbers(l1=l1_a, l2=l2_a)
+        print(result)
+
+
+
+        l1_b = ListNode(8, None)
+        l1_a = ListNode(1, l1_b)
+
+        l2_a = ListNode(0, None)
         
-        l3 = None
-        for i in reversed(range(0, len(c))):
-            newNode = ListNode(val=int(c[i]), next=l3)
-            l3 = newNode
-        
-        searching = True
-        a = l3
-        while searching:
-            print(a.val)
-            if a.next == None:
-                searching = False
-            a = a.next
 
-        # print('result:')
-        # print(l3.val)
-        # print(l3.next.val)
-        # print(l3.next.next.val)
-        return l3
+        solution = Solution()
+        result = solution.addTwoNumbers(l1=l1_a, l2=l2_a)
+        print(result)
+    
 
-
-
-l1_c = ListNode(2, None)
-l1_b = ListNode(4, l1_c)
-l1_a = ListNode(3, l1_b)
-
-l2_c = ListNode(5, None)
-l2_b = ListNode(6, l2_c)
-l2_a = ListNode(4, l2_b)
-
-solution = Solution()
-print(solution.addTwoNumbers(l1=l1_a, l2=l2_a))
+if __name__=='__main__':
+    unittest.main()
