011a9500cd3403fcd8826bfccd5c736c0135253f
Andre Doumad <andre@stringkeeper.com>
2020-08-10 00:50:23 -0700

    Andre Doumad

--- maximumSubarray.py
+++ maximumSubarray.py
@@ -18,3 +18,27 @@ try coding another solution using the divide and conquer approach, which is more
 
 class Solution:
     def maxSubArray(self, nums):
+        maxsum = nums[0]
+        for i in range(1, len(nums)):
+            if nums[i-1] > 0:
+                nums[i] += nums[i-1]
+            maxsum = max(nums[i], maxsum)
+            print('nums[', i-1, '] ', nums[i-1], ' maxsum: ', maxsum)
+        return maxsum
+
+solution = Solution()
+result = solution.maxSubArray([-2,1,-3,4,-1,2,1,-5,4])
+print('result is ', result)
+
+'''
+output:
+nums[ 0 ]  -2  maxsum:  1
+nums[ 1 ]  1  maxsum:  1
+nums[ 2 ]  -2  maxsum:  4
+nums[ 3 ]  4  maxsum:  4
+nums[ 4 ]  3  maxsum:  5
+nums[ 5 ]  5  maxsum:  6
+nums[ 6 ]  6  maxsum:  6
+nums[ 7 ]  1  maxsum:  6
+result is  6
+'''
