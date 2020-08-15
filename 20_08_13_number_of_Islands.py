deb782473d73d6849b0c36c73cbfbb6952313ab7
Andre Doumad <andre@stringkeeper.com>
2020-08-15 00:02:21 -0700

    Andre Doumad

--- 20_08_13_number_of_Islands.py
+++ 20_08_13_number_of_Islands.py
@@ -30,10 +30,10 @@ import unittest, typing
 
 class Solution:
     def numIslands(self, grid):
-        print('==========')
-        print('Finding number of islands given:')
-        for k in range(len(grid)):
-            print(grid[k])
+        # print('==========')
+        # print('Finding number of islands given:')
+        # for k in range(len(grid)):
+        #     print(grid[k])
         if not grid:
             return 0
             
@@ -41,17 +41,17 @@ class Solution:
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == '1':
-                    print('---before---')
-                    for k in range(len(grid)):
-                        print(grid[k])
+                    # print('---before---')
+                    # for k in range(len(grid)):
+                    #     print(grid[k])
                     # erase connected islands
                     self.dfs(grid, i, j)
                     # then count it as one island
                     count += 1
-                    print('---after---')
-                    for k in range(len(grid)):
-                        print(grid[k])
-                    print('islands: ', count)
+                    # print('---after---')
+                    # for k in range(len(grid)):
+                    #     print(grid[k])
+                    # print('islands: ', count)
         return count
 
     def dfs(self, grid, i, j):
