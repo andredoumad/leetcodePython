44d8210cf49527e39797230bd1c8f581fcac43b6
Andre Doumad <andre@stringkeeper.com>
2020-08-14 23:38:02 -0700

    Andre Doumad

--- incomplete/20_08_13_number_of_Islands.py
+++ incomplete/20_08_13_number_of_Islands.py
@@ -1,5 +1,4 @@
 # Andre Doumad
-#TODO This needs refactoring and further tests. It passes all but a few corner cases.
 '''
 This Problem was asked by Amazon.
 
@@ -31,23 +30,25 @@ import unittest, typing
 
 class Solution:
     def numIslands(self, grid):
+        if not grid:
+            return 0
+            
         count = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == '1':
-                    print(i,j)
-                    self.dfs(grid,i,j)
-                    count  += 1
-        #print(grid)
+                    self.dfs(grid, i, j)
+                    count += 1
         return count
-    # use a helper function to flip connected '1's to 0
-    def dfs(self,grid,i,j):
-        grid[i][j] = 0
-        for dr,dc in (1,0), (-1,0), (0,-1), (0,1):
-            r = i + dr
-            c = j + dc
-            if 0 <= r < len(grid) and 0 <= c < len(grid[0]) and grid[r][c]=='1':
-                self.dfs(grid,r,c)
+
+    def dfs(self, grid, i, j):
+        if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] != '1':
+            return
+        grid[i][j] = '#'
+        self.dfs(grid, i+1, j)
+        self.dfs(grid, i-1, j)
+        self.dfs(grid, i, j+1)
+        self.dfs(grid, i, j-1)
 
 
 class unitTest(unittest.TestCase):
@@ -60,6 +61,7 @@ class unitTest(unittest.TestCase):
             ["0","0","0","0","0"]
             ])
         print('NUMBER OF ISLANDS: ', result)
+        self.assertEqual(result, 1)
 
         solution = Solution()
         result = solution.numIslands(grid=[
@@ -69,19 +71,24 @@ class unitTest(unittest.TestCase):
             ["0","0","0","1","1"]
             ])
         print('NUMBER OF ISLANDS: ', result)
+        self.assertEqual(result, 3)
 
         solution = Solution()
-        result = solution.numIslands(grid=[
-            ])
+        result = solution.numIslands(grid=[])
         print('NUMBER OF ISLANDS: ', result)
+        self.assertEqual(result, 0)
 
         solution = Solution()
         result = solution.numIslands(grid=[["1"]])
         print('NUMBER OF ISLANDS: ', result)
+        self.assertEqual(result, 1)
 
         solution = Solution()
         result = solution.numIslands(grid=[["1","1"]])
         print('NUMBER OF ISLANDS: ', result)
+        self.assertEqual(result, 1)
+
+
 if __name__=='__main__':
     unittest.main()
 
