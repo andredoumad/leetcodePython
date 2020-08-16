a2dbcc6cd45cadf377304610c4c29b8fb881c698
tr3b <andre@stringkeeper.com>
2020-08-16 15:24:39 -0700

    Andre Doumad

---
+++ incomplete/20_08_16_valid_palindrome.py
@@ -0,0 +1,38 @@
+# Andre Doumad
+'''
+125. Valid Palindrome
+Easy
+
+1341
+
+3073
+
+Add to List
+
+Share
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+Example 1:
+
+Input: "A man, a plan, a canal: Panama"
+Output: true
+Example 2:
+
+Input: "race a car"
+Output: false
+ 
+
+Constraints:
+
+s consists only of printable ASCII characters.
+'''
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        pass
+
+
+solution= Solution()
+# solution.solve
