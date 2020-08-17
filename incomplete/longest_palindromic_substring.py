ee602da5e55e162bd68364de3a05f9c6503bbe73
tr3b <andre@stringkeeper.com>
2020-08-16 19:17:52 -0700

    Andre Doumad

--- incomplete/longest_palindromic_substring.py
+++ incomplete/longest_palindromic_substring.py
@@ -1,6 +1,7 @@
 # Andre Doumad
 '''
-Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+Given a string s, find the longest palindromic substring in s. 
+You may assume that the maximum length of s is 1000.
 
 Example 1:
 Input: "babad"
