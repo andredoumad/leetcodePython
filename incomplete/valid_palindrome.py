1b987183f6d0e044ddd88f09f6ae3a5ac2949a74
Andre Doumad <andre@stringkeeper.com>
2020-08-16 17:18:49 -0700

    Andre Doumad

--- incomplete/valid_palindrome.py
+++ incomplete/valid_palindrome.py
@@ -28,11 +28,30 @@ Constraints:
 
 s consists only of printable ASCII characters.
 '''
-
+import re
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        pass
+        if len(s) == 0:
+            return True
+        else:
+            s = s.replace(' ', '')
+            s = re.sub(r'\W+', '', s)
+            s = s.lower()
+            # s = filter(str.isalnum, s)
+        print(s)
+        # exit()
+        i, j = 0, len(s)-1
+        while i < j:
+            if s[i] == s[j]:
+                pass
+
 
 
 solution= Solution()
-# solution.solve
+result = solution.isPalindrome("A man, a plan, a canal: Panama")
+print('result', result)
+# assert(result, True)
+
+result = solution.isPalindrome("race a car")
+print('result', result)
+# assert(result, False)
