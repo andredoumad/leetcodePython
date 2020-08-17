7bd185285f8bcb1ea417b9f34fa68e6ed4e0e5dc
Andre Doumad <andre@stringkeeper.com>
2020-08-16 21:31:19 -0700

    Andre Doumad

--- incomplete/longest_palindromic_substring.py
+++ incomplete/longest_palindromic_substring.py
@@ -14,9 +14,34 @@ Output: "bb"
 '''
 
 class Solution:
+    # starting at l,r expand outwards and find the palindrome
+    def test(self, s, l, r):
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l -= 1
+            r += 1
+        return s[l+1:r]
+
     def longestPalindrome(self, s: str) -> str:
-        pass
+        res = ""
+        for i in range(len(s)):
+            odd  = self.test(s, i, i)
+            even = self.test(s, i, i+1)
+            res = max(res, odd, even, key=len)
+        return res
 
 solution = Solution()
-solution.longestPalindrome('babad')
-solution.longestPalindrome('cbbd')
+result = solution.longestPalindrome('babad')
+print('result ', result)
+
+result = solution.longestPalindrome('cbbd')
+print('result ', result)
+
+result = solution.longestPalindrome('bb')
+print('result ', result)
+
+
+result = solution.longestPalindrome('abb')
+print('result ', result)
+
+result = solution.longestPalindrome('ccd')
+print('result ', result)
