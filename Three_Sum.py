f803420fe04d319695e03a053c1ec6b70f5b9953
Andre Doumad <andre@stringkeeper.com>
2020-08-19 23:58:07 -0700

    Andre Doumad

--- Three_Sum.py
+++ Three_Sum.py
@@ -1,6 +1,8 @@
 # Andre Doumad
 # 200817
 '''
+This problem was asked by Amazon.
+
 Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
 
 Note:
