96ee70b179fe87967040bb94b155b0748f2e4478
Andre Doumad <andre@stringkeeper.com>
2020-08-20 19:15:25 -0700

    Andre Doumad

--- incomplete/Subarray_Sum_Equals_K.py
+++ incomplete/Subarray_Sum_Equals_K.py
@@ -8,13 +8,29 @@ Example 1:
 Input:nums = [1,1,1], k = 2
 Output: 2
  
-
 Constraints:
-
 The length of the array is in range [1, 20,000].
 The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].
 '''
-
 class Solution:
-    def subarraySum(self, nums: List[int], k: int) -> int:
-        
+    def subarraySum(self, nums, k):
+        sums = {0:1}
+        t=s=0
+        for num in nums:
+            s += num
+            t += sums.get(s-k,0)
+            sums[s] = sums.get(s,0)+1
+        return t
+solution = Solution()
+print(solution.subarraySum([1,1,1], 2))
+print(solution.subarraySum([1,2,3], 3))
+print(solution.subarraySum([1,2,1,2,1], 3))
+print(solution.subarraySum([-1,-1,1], 0))
+
+'''
+output:
+2
+2
+4
+1
+'''
