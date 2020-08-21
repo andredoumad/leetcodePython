cf09b039690161dbb22dafae979c176266e6ecb1
tr3b <andre@stringkeeper.com>
2020-08-21 13:05:55 -0700

    Andre Doumad

---
+++ generate_parentheses.py
@@ -0,0 +1,37 @@
+# Andre Doumad
+# 200821
+'''
+This problem was asked by Amazon.
+
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+For example, given n = 3, a solution set is:
+
+[
+  "((()))",
+  "(()())",
+  "(())()",
+  "()(())",
+  "()()()"
+]
+'''
+from typing import List
+class Solution:
+    def dfs(self, leftRemain, rightRemain, path, res):
+        if leftRemain > rightRemain or leftRemain < 0 or rightRemain < 0:
+            return
+        if leftRemain == 0 and rightRemain == 0:
+            res.append(path)
+        self.dfs(leftRemain-1, rightRemain, path+"(", res)
+        self.dfs(leftRemain, rightRemain-1, path+")", res)
+
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        self.dfs(n,n,"",res)
+        return res
+
+s = Solution()
+print(s.generateParenthesis(3))
+'''
+['((()))', '(()())', '(())()', '()(())', '()()()']
+'''
