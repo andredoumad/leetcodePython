b23e3ff69e82f1cbc03ee025f0c70a48a105cf30
tr3b <andre@stringkeeper.com>
2020-08-21 17:10:03 -0700

    Andre Doumad

--- incomplete/Container_With_Most_Water.py
+++ incomplete/Container_With_Most_Water.py
@@ -15,6 +15,41 @@ Output: 49
 from typing import List
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+        i, j = 0, len(height)-1
+        water = 0
+        while i < j:
+            water = max(water, (j-i) * min(height[i], height[j]))
+            if height[i] < height[j]:
+                i+=1
+            else:
+                j-=1
+        return water
+
+
+s = Solution()
+print(s.maxArea([1,8,6,2,5,4,8,3,7]))
+'''
+49
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
         i, j = 0, len(height) - 1
         water = 0
         while i < j:
@@ -24,9 +59,4 @@ class Solution:
             else:
                 j -= 1
         return water
-
-s = Solution()
-print(s.maxArea([1,8,6,2,5,4,8,3,7]))
-'''
-49
 '''
