84914a68cbf2df8a4ebec645b0e7ee94b005143b
tr3b <andre@stringkeeper.com>
2020-08-22 20:41:50 -0700

    Andre Doumad

--- incomplete/Next_Permutation.py
+++ incomplete/Next_Permutation.py
@@ -15,14 +15,33 @@ Here are some examples. Inputs are in the left-hand column and its corresponding
 '''
 from typing import List
 class Solution:
+
     def nextPermutation(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        pass
-
+        i = j = len(nums)-1
+        while i > 0 and nums[i-1] >= nums[i]:
+            i -= 1
+        if i == 0:   # nums are in descending order
+            nums.reverse()
+            return nums
+        k = i - 1    # find the last "ascending" position
+        while nums[j] <= nums[k]:
+            j -= 1
+        nums[k], nums[j] = nums[j], nums[k]  
+        l, r = k+1, len(nums)-1  # reverse the second part
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l +=1 ; r -= 1
+        return nums
 
 s = Solution()
 print(s.nextPermutation([1,2,3]))
 print(s.nextPermutation([3,2,1]))
 print(s.nextPermutation([1,1,5]))
+'''
+[1, 3, 2]
+[1, 2, 3]
+[1, 5, 1]
+'''
