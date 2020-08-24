52b84e5a8e15bd22bdecb83fdd8b26b64a5b6cf1
Andre Doumad <andre@stringkeeper.com>
2020-08-24 00:37:14 -0700

    Andre Doumad

--- incomplete/Largest_item_association.py
+++ incomplete/Largest_item_association.py
@@ -46,88 +46,122 @@ class PairString():
         self.first = first
         self.second = second
 
-class Graph():
-    def __init__(self):
-        self.adj = {}
-        self.nameToInt = {}
-        self.intToName = {}
-
-    def addEdge(self, v, e):
-        if v not in self.adj:
-            self.adj[v] = [e]
-        else:
-            self.adj[v].append(e)
-
-    def printGraph(self):
-        print('--- graph ---- ')
-        print('length of graph is ', len(self.adj))
-        for k,v in self.adj.items():
-            print('k ', k, ' v ', v)
-
-        print('--- nameToInt ---- ')
-        print('length of nameToInt is ', len(self.nameToInt))
-        for k,v in self.nameToInt.items():
-            print('k ', k, ' v ', v)
-
-        print('--- intToName ---- ')
-        print('length of intToName is ', len(self.intToName))
-        for k,v in self.intToName.items():
-            print('k ', k, ' v ', v)
-
-    def dfs(self, start):
-        print('start ', start)
-        self.doDfs(start, {start:True}, [start], self.adj)
-
-    def doDfs(self, start, visited, queue, adj):
-        print('dfs start: ', start)
-        print('queue start: ', queue)
-        while len(queue) > 0:
-            v = queue.pop()
-            while len(adj) > 0:
-                e = adj[v].pop()
-                
-                if e not in visited:
-                    visited[e] = True
-                    print(v, ' is connected to ', e)
-
 class Solution():
     def largestItemAssociation(self, itemAssociation):
-        print('------------------')
-        print('INPUT: ', itemAssociation)
-        graph = Graph()
-        
-        itr = 1
+        knownItems = []
+        item_map = {}
+        grid = []
         for i in range(0, len(itemAssociation)):
-            print('itemAssociation ', itemAssociation[i])
-            if itemAssociation[i].first not in graph.nameToInt: 
-                graph.nameToInt[itemAssociation[i].first] = itr
-                graph.intToName[itr] = itemAssociation[i].first
-                graph.addEdge(itemAssociation[i].first, i)
-                itr+=1
-            else:
-                graph.addEdge(graph.nameToInt[itemAssociation[i].first], i)
+            if itemAssociation[i].first not in knownItems:
+                knownItems.append(itemAssociation[i].first)
+            if itemAssociation[i].second not in knownItems:
+                knownItems.append(itemAssociation[i].second)
+        print('known items ', knownItems)
 
-            if itemAssociation[i].second not in graph.nameToInt: 
-                graph.nameToInt[itemAssociation[i].second] = itr
-                graph.intToName[itr] = itemAssociation[i].second
-                graph.addEdge(itemAssociation[i].second, i)
-                itr+=1
-            else:
-                graph.addEdge(graph.nameToInt[itemAssociation[i].second], i)
+        for i in range(0, len(itemAssociation)):
+            grid.append([])
+            for j in range(0, len(knownItems)):
+                if knownItems[j] == itemAssociation[i].first:
+                    grid[i].append('1')
+                    item_map[i,j] = itemAssociation[i].first 
+                elif knownItems[j] == itemAssociation[i].second:
+                    grid[i].append('1')
+                    item_map[i,j] = itemAssociation[i].second 
+                else:
+                    grid[i].append('0')
 
-        graph.printGraph()
-        graph.dfs(itemAssociation[0].first)
+        for i in range(0, len(grid)):
+            print(grid[i])
 
-        res = True
+        numIslands = 0
+        pairs = {}
+        for i in range(0, len(grid)):
+            for j in range(0, len(grid[0])):
+                if grid[i][j] == '1':
+                    pairs[numIslands] = []
+                    self.dfs(grid, i, j, numIslands, pairs)
+                    numIslands += 1
+                    # pairs.append(p) 
+
+        print('numIslands ', numIslands)
+        biggestGroup = 0
+        for k,v in pairs.items():
+            print('group ', k, ' pairs ', v)
+            if biggestGroup < len(v):
+                biggestGroup = k
+        print('biggest group is ', biggestGroup)
+        res = []
+        for coordinate in pairs[biggestGroup]:
+            print(coordinate)
+            print('item_map: ', item_map[coordinate[0],coordinate[1]])
+            if item_map[coordinate[0],coordinate[1]] not in res:
+                res.append(item_map[coordinate[0],coordinate[1]])
         print('RESULT: ', res)
-        print('------------------')
         return res
+
+    def dfs(self, grid, i, j,  numIslands, pairs):
+        if i < 0 or j < 0 or i >=len(grid) or j >= len(grid[0]) or grid[i][j] != '1':
+            return
+        grid[i][j] = '#'
+        pairs[numIslands].append([i,j])
+        self.dfs(grid, i+1, j, numIslands,  pairs) # down
+        self.dfs(grid, i-1, j,  numIslands, pairs) # up
+        self.dfs(grid, i, j-1,  numIslands, pairs) # left
+        self.dfs(grid, i, j+1,  numIslands, pairs) # right
+
 
 
 s = Solution()
-# print(s.largestItemAssociation([]) == [])
 print(s.largestItemAssociation([PairString('Item1', 'Item2'), PairString('Item3', 'Item4'), PairString('Item4', 'Item5')]) == ['Item3', 'Item4', 'Item5'])
-# print(s.largestItemAssociation([['item1','item2']]) == ['item1','item2'])
+print(s.largestItemAssociation([PairString('item1', 'item2')]) == ['item1','item2'])
+'''
+OUTPUT:
+['1', '1', '0', '0', '0']
+['0', '0', '1', '1', '0']
+['0', '0', '0', '1', '1']
+numIslands  2
+group  0  pairs  [[0, 0], [0, 1]]
+group  1  pairs  [[1, 2], [1, 3], [2, 3], [2, 4]]
+biggest group is  1
+[1, 2]
+item_map:  Item3
+[1, 3]
+item_map:  Item4
+[2, 3]
+item_map:  Item4
+[2, 4]
+item_map:  Item5
+RESULT:  ['Item3', 'Item4', 'Item5']
+True
+known items  ['item1', 'item2']
+['1', '1']
+numIslands  1
+group  0  pairs  [[0, 0], [0, 1]]
+biggest group is  0
+[0, 0]
+item_map:  item1
+[0, 1]
+item_map:  item2
+RESULT:  ['item1', 'item2']
+True
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# print(s.largestItemAssociation([]) == [])
 # print(s.largestItemAssociation([['item1','item2'],['item2','item3'],['item4','item5'],['item5','item6']]) == ['item1','item2','item3'])
 # print(s.largestItemAssociation([['Item1','Item2'],['Item3','Item4'],['Item4','Item5']]) == ['Item3', 'Item4', 'Item5'])
 # print(s.largestItemAssociation([['Item1','Item2'],['Item2','Item5'],['Item3']]) == ['Item1', 'Item2', 'Item5'])
