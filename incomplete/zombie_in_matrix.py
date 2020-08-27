acf627b292b1f60af02a0f38eab7c4f44cb21360
tr3b <andre@stringkeeper.com>
2020-08-27 12:46:31 -0700

    Andre Doumad

--- incomplete/zombie_in_matrix.py
+++ incomplete/zombie_in_matrix.py
@@ -1,5 +1,6 @@
 # Andre Doumad
 # 200827
+
 '''
 Given a 2D grid, each cell is either a zombie 1 or a human 0. Zombies can turn adjacent (up/down/left/right) human beings into zombies every hour. Find out how many hours does it take to infect all humans?
 
@@ -29,6 +30,5 @@ At the end of the 2nd hour, the status of the grid:
 int minHours(int rows, int columns, List<List<Integer>> grid) {
 	// todo
 }
-
 '''
 
