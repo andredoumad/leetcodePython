976e76f36ecd0c3987a6c2cb48ed6736a421d663
tr3b <andre@stringkeeper.com>
2020-08-30 14:59:43 -0700

    Andre Doumad

--- incomplete/Substrings_of_size_K_with_K_distinct_chars.py
+++ incomplete/Substrings_of_size_K_with_K_distinct_chars.py
@@ -30,14 +30,21 @@ class Solution():
     def solve(self, s, k):
         if not s or k == 0:
             return []
-        
-
+        letter, res = {}, set()
+        start = 0
+        for i in range(len(s)):
+            if s[i] in letter and letter[s[i]] >= start:
+                start = letter[s[i]]+1
+            letter[s[i]] = i
+            if i-start+1 == k:
+                res.add(s[start:i+1])
+                start +=1
+        return list(res)
 
 class UnitTest(unittest.TestCase):
     def test_a(self):
         s = Solution()
         result = s.solve('abcabc', 3)
-        self.assertEqual(result, ["abc", "bca", "cab"])
 
 if __name__ == '__main__':
     unittest.main()
