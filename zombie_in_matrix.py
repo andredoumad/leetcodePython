206045fe9def8ec0e47860b839b75812bf999548
tr3b <andre@stringkeeper.com>
2020-08-29 21:05:03 -0700

    Andre Doumad

--- zombie_in_matrix.py
+++ zombie_in_matrix.py
@@ -32,29 +32,28 @@ At the end of the 2nd hour, the status of the grid:
 
 '''
 
-
 class Solution():
     def minHours(self, grid):
         if not grid: return 0
-        zombies = [] # hours, row, col
         nrow, ncol = len(grid), len(grid[0])
-        directions = [[1,0],[-1,0],[0,1],[0,-1]]
         hours = 0
+        directions = [[0,1],[0,-1],[-1,0],[1,0]]
+        zombies = [] # hour, row, col
         for row in range(nrow):
             for col in range(ncol):
                 if grid[row][col] == 1:
-                    zombies.append([0,row,col])
+                    zombies.append([0, row, col])  
         while zombies:
+            print('zombies: ', zombies)
             hours, x, y = zombies.pop(0)
+            print('zombies: ', zombies)
             for direction in directions:
                 X = x+direction[0]
                 Y = y+direction[1]
                 if 0<=X<nrow and 0<=Y<ncol and grid[X][Y] == 0:
                     grid[X][Y] = 1
                     zombies.append([hours+1, X, Y])
-
         return hours
-
 
 s = Solution()
 print(s.minHours([
