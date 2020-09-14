2d5b7a6e850f571a0d74ac35039b9bf71c40da6b
Andre Doumad <andre@stringkeeper.com>
2020-09-14 16:51:53 -0700

    Andre Doumad

--- incomplete/two_sum_4_input_is_bst.py
+++ incomplete/two_sum_4_input_is_bst.py
@@ -32,24 +32,46 @@ Output: False
 '''
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
     def findTarget(self, root: TreeNode, k: int) -> bool:
-        if not root:
-            return False
-        return self._findTarget(root, set(), k)
-        
-    def _findTarget(self, node, nodes, k):
-        if not node:
-            return False
-        
-        complement = k-node.val
-        
-        if complement in nodes:
-            return True
-        nodes.add(node.val)
-        return self._findTarget(node.left, nodes, k) or self._findTarget(node.right, nodes, k)
+        s = set()
+        def helper(root, k):
+            print(s)
+            if not root: return False
+            print('root.val ', root.val )
+            print('k - root.val ', k - root.val)
+            if k - root.val in s: return True
+            s.add(root.val)
+            return(helper(root.left, k) or helper(root.right, k))
+        return helper(root, k)
+
+
+s = Solution()
+
+t = TreeNode(5,TreeNode(3, TreeNode(2), TreeNode(4)),TreeNode(6,None,TreeNode(7)))
+
+print(s.findTarget(t, 9))
+
+'''
+output:
+set()
+root.val  5
+k - root.val  4
+{5}
+root.val  3
+k - root.val  6
+{3, 5}
+root.val  2
+k - root.val  7
+{2, 3, 5}
+{2, 3, 5}
+{2, 3, 5}
+root.val  4
+k - root.val  5
+True
+'''
